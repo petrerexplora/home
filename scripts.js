ca3dbc8d9139fa1a0e31cf77d5ae85cbeaeb60dd
petrerexplora <jebusquier@gmail.com>
2024-07-30 17:43:30 +0200

    HOMME

--- scripts.js
+++
@@ -1,6 +0,0 @@
-function changeLanguage() {
-    const lang = document.getElementById('language').value;
-    // Aquí puedes implementar el cambio de idioma, por ejemplo, cargando el contenido adecuado
-    // o usando una biblioteca de internacionalización.
-    alert("Idioma cambiado a: " + lang);
-}
